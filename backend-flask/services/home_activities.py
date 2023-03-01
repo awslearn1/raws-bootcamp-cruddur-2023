e7930ae7368688fdc11c6a2af3b35272364d96f3
awslearn1 <revathijoshi70+awslearn@gmail.com>
2023-03-01 23:53:33 +0000

    adding

--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -10,7 +10,7 @@ class HomeActivities:
       span = trace.get_current_span()
       now = datetime.now(timezone.utc).astimezone()
       span.set_attribute("user.id", user.id())
-      #span.set_attribute("app.now", now.isoformat())
+      span.set_attribute("app.now", now.isoformat())
       results = [{
        'uuid': '68f126b0-1ceb-4a33-88be-d90fa7109eee',
        'handle':  'andrew brown',
