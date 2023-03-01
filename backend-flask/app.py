1cd1439fe927b4a5ad80edf6cb84e7d33c426997
awslearn1 <revathijoshi70+awslearn@gmail.com>
2023-03-01 22:07:15 +0000

    instrument honeycomb

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -21,7 +21,7 @@ from opentelemetry.instrumentation.requests import RequestsInstrumentor
 from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
 from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import BatchSpanProcessor
-from opentelemetry.sdk.trace.export import ConsoleSpanExporter, SimpleSpanProcessor
+#from opentelemetry.sdk.trace.export import ConsoleSpanExporter, SimpleSpanProcessor
 
 # Honeycomb -------
 # Honeycomb - Initialize tracing and an exporter that can send data to Honeycomb
@@ -30,8 +30,8 @@ processor = BatchSpanProcessor(OTLPSpanExporter())
 provider.add_span_processor(processor)
 
 # Honeycomb - Show spans in the backend flask in the console standard output
-simple_processor = SimpleSpanProcessor(ConsoleSpanExporter())
-provider.add_span_processor(simple_processor)
+#simple_processor = SimpleSpanProcessor(ConsoleSpanExporter())
+#provider.add_span_processor(simple_processor)
 
 trace.set_tracer_provider(provider)
 tracer = trace.get_tracer(__name__)
