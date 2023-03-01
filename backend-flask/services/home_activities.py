4b332f0fadee739579471f6df9d1ae58ece109df
awslearn1 <revathijoshi70+awslearn@gmail.com>
2023-03-01 23:22:14 +0000

    adding

--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -7,12 +7,12 @@ tracer = trace.get_tracer("home.activities")
 class HomeActivities:
   def run():
     with tracer.start_as_current_span("home-activities-mock-data"):
-      span = trace.get_current_span()
+      #span = trace.get_current_span()
       now = datetime.now(timezone.utc).astimezone()
-      span.set_attribute("app.now", now.isoformat())
+      #span.set_attribute("app.now", now.isoformat())
       results = [{
-      'uuid': '68f126b0-1ceb-4a33-88be-d90fa7109eee',
-      'handle':  'andrew brown',
+       'uuid': '68f126b0-1ceb-4a33-88be-d90fa7109eee',
+       'handle':  'andrew brown',
       'message': 'cloud is very fun!',
       'created_at': (now - timedelta(days=2)).isoformat(),
       'expires_at': (now + timedelta(days=5)).isoformat(),
