cc71a2f47da6f535581494d49bbce310a26fef6d
awslearn1 <revathijoshi70+awslearn@gmail.com>
2023-03-01 23:02:35 +0000

    honeycomb update

--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -10,24 +10,24 @@ class HomeActivities:
       #span = trace.get_current_span()
       now = datetime.now(timezone.utc).astimezone()
       #span.set_attribute("app.now", now.isoformat())
-      results = [{
-        'uuid': '68f126b0-1ceb-4a33-88be-d90fa7109eee',
-        'handle':  'andrew Brown',
-        'message': 'cloud is very fun!',
-        'created_at': (now - timedelta(days=2)).isoformat(),
-        'expires_at': (now + timedelta(days=5)).isoformat(),
-        'likes_count': 5,
-        'replies_count': 1,
+       results = [{
+      'uuid': '68f126b0-1ceb-4a33-88be-d90fa7109eee',
+      'handle':  'andrew brown',
+      'message': 'cloud is very fun!',
+      'created_at': (now - timedelta(days=2)).isoformat(),
+      'expires_at': (now + timedelta(days=5)).isoformat(),
+      'likes_count': 5,
+      'replies_count': 1,
+      'reposts_count': 0,
+      'replies': [{
+        'uuid': '26e12864-1c26-5c3a-9658-97a10f8fea67',
+        'reply_to_activity_uuid': '68f126b0-1ceb-4a33-88be-d90fa7109eee',
+        'handle':  'worf',
+        'message': 'this post has no honor!',
+        'likes_count': 0,
+        'replies_count': 0,
         'reposts_count': 0,
-        'replies': [{
-          'uuid': '26e12864-1c26-5c3a-9658-97a10f8fea67',
-          'reply_to_activity_uuid': '68f126b0-1ceb-4a33-88be-d90fa7109eee',
-          'handle':  'worf',
-          'message': 'this post has no honor!',
-          'likes_count': 0,
-          'replies_count': 0,
-          'reposts_count': 0,
-          'created_at': (now - timedelta(days=2)).isoformat()
+        'created_at': (now - timedelta(days=2)).isoformat()
       }],
     },
     {
@@ -42,12 +42,12 @@ class HomeActivities:
     {
       'uuid': '248959df-3079-4947-b847-9e0892d1bab4',
       'handle':  'garek',
-      'message': 'my dear doctor, I am just simple tailor',
+      'message': 'my dear doctor, i am just simple tailor',
       'created_at': (now - timedelta(hours=1)).isoformat(),
       'expires_at': (now + timedelta(hours=12)).isoformat(),
       'likes': 0,
       'replies': []
     }
     ]
-    span.set_attribute("app.result_length", len(results))    
+    span.set_attribute("app.result_length", len(results))  
     return results
