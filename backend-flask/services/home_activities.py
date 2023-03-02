bab2f3b8ae0acf2e8f6411b6a014d066fb1e629b
awslearn1 <revathijoshi70+awslearn@gmail.com>
2023-03-02 00:00:28 +0000

    adding

--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -3,14 +3,12 @@ from datetime import datetime, timedelta, timezone
 from opentelemetry import trace
 
 tracer = trace.get_tracer("home.activities")
-tracer = trace.get_tracer(__name__)
 
 class HomeActivities:
   def run():
     with tracer.start_as_current_span("home-activities-mock-data"):
       span = trace.get_current_span()
       now = datetime.now(timezone.utc).astimezone()
-      span.set_attribute("user.id", user.id())
       span.set_attribute("app.now", now.isoformat())
       results = [{
        'uuid': '68f126b0-1ceb-4a33-88be-d90fa7109eee',
